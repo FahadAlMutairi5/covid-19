9d79b3b8ab3b460bdaae696cbd810e067aa4c97d
FahadAlMutairi5 <fahadalmutairi@MacBook-Pro-alkhas-b-Fahad.local>
2020-08-01 15:47:49 +0300

    Done

--- src/components/hestory/index.js
+++ src/components/hestory/index.js
@@ -69,7 +69,7 @@ class index extends React.Component {
     // console.log(lastSt)
     let lastmanth = cityHestoryData && cityHestoryData.map(
         his => his
-    ).filter(fil => fil.lastUpdatedAtApify > `2020-${month}-01T00:00:00.000Z`)
+    ).filter(fil => fil.lastUpdatedAtApify > `2020-08-01T00:00:00.000Z`)
     
     // if (fullDataHis2){
     //     for (let i=0 ; i<fullDataHis2.length; i++){
